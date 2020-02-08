0d85575697b230552f8b92b7e11813ba7e76bc8a
sterg <sterg@teiemt.gr>
2020-02-08 16:51:49 +0200

    minor update

--- Linux64GroovyLab.sh
+++ Linux64GroovyLab.sh
@@ -1,1 +1,3 @@
-java  -XX:+UseNUMA -XX:+UseParallelGC -XX:+UseCompressedOops    -Djava.library.path=.:./lib -Xss5m -Xms2000m -Xmx25000m -jar GroovyLab.jar
+
+export LD_LIBRARY_PATH=$PWD/lib:$PWD/defaultToolboxes:$LD_LIBRARY_PATH
+java  -XX:+UseNUMA -XX:+UseParallelGC -XX:+UseCompressedOops    -Djava.library.path=.:./lib:./defaultToolboxes  -Xss5m -Xms2000m -Xmx25000m -jar GroovyLab.jar
--- Win64RunGroovyLab.bat
+++ Win64RunGroovyLab.bat
@@ -1,1 +1,4 @@
+
+SET PATH=%PATH%;%cd%\lib\
+
 java    -XX:+TieredCompilation -XX:+UseNUMA -XX:+UseParallelGC -Djava.library.path=./lib;. -Xss5m -Xms3000m -Xmx6500m  -jar GroovyLab.jar
